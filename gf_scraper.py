0645afdd76522f7b08c330b9c1e2cf2122c75ca4
Loewetiger <leon.2000@hotmail.de>
2020-03-29 15:35:30 +0200

    Added progress bar, minor "optimisation"

--- gf_scraper.py
+++ gf_scraper.py
@@ -1,21 +1,25 @@
 from bs4 import BeautifulSoup
 import urllib.request
+from tqdm import tqdm
 
 output_file = "output.txt"  # ouput file
 tag = "neue"  # tag (https://www.gutefrage.net/beliebte_tags or "neue")
 pages = 1  # amount of pages getting scraped (100 questions per page)
 
-it = 1
+page = []
 
 outfile = open(output_file, 'w', encoding="utf-8")
-while it <= pages:
+print("Appending pages")
+for n in tqdm(range(0, pages)):
     with urllib.request.urlopen("https://www.gutefrage.net/tag/" + tag + "/"
-                                + str(it)) as url:
-        page = url.read()
-    soup = BeautifulSoup(page, 'html.parser')
+                                + str(n + 1)) as url:
+        page.append(url.read())
+print("Getting text and writing to file")
+for p in tqdm(page):
+    soup = BeautifulSoup(p, 'html.parser')
     only = soup.find_all("div",
                          {'class': 'H4 Question-title u-big u-mbm'})[:100]
     for i in only:
         outfile.write(i.get_text() + "\n")
-    it = it + 1
 outfile.close()
+print("Finished!")
